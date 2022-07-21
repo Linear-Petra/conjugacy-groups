5f76dd45f1c1390174222354452a7b482c41c847
Morgan <linear@tutanota.de>
2022-07-21 11:56:55 -0500

    Fix to isSL, shortlexify
    Allows dealing with c- at the end, instead of just c

--- conjugacyF_2xZ.py
+++ conjugacyF_2xZ.py
@@ -11,14 +11,14 @@
 import re
 import conj_lib as cl
 
-S = ["a,b,c"]
+S = ['a','b','c']
 
 # Determine if a word is ShortLex
 def isSL(w):
     if not cl.verify(w):
         raise ValueError("Not a valid word")
 
-    return re.match(r'^(?!.*?([a-zA-Z])(\1-|-\1(?!-)))((a-?)*(b-?)*)*c*$',w) != None
+    return re.match(r'^(?!.*?([a-zA-Z])(\1-|-\1(?!-)))((a-?)*(b-?)*)*(c-?)*$',w) != None
     #Something of the form (a^{i_k} b^{j_k})^k c^l) for k,l,i_k,j_k in Z
     #Equivalent to
     #return (cl.reduce(w) == w) and (re.match(r'^((a-?)*(b-?)*)*c*$',w) != None)
@@ -26,8 +26,9 @@ def isSL(w):
 #Reduces a word and makes it so isSL(w) == True
 def shortlexify(w):
     if not cl.verify(w):
-        raise ValueError("Not a valid word")
-    c_count = w.count('c')
-    w_sl = w.replace('c','') + 'c'*c_count #c commutes with all
+        raise ValueError(w,"is not a valid word")
+    cm_count = w.count('c-')
+    c_count = w.count('c') - cm_count
+    w_sl = re.sub('c-?','',w) + 'c'*c_count + 'c-'*cm_count #c commutes with all
     w_sl = cl.reduce(w_sl)
     return w_sl
